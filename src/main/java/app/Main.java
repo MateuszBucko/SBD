2591d1d47ffafa2ca0a126671a2175de3cab8f68
Mateusz <mateuszbucko1@gmail.com>
2016-11-23 00:34:07 +0100

    ref

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -14,24 +14,21 @@ public class Main {
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		
+
 		entityManager.getTransaction().begin();
 		Administrator administrator = new Administrator();
 		administrator.setDataZatrudnienia(new Date());
-		
+
 		AdministratorSzczegoly administratorSzczegoly = new AdministratorSzczegoly("Jan", "Kowalski", "Prosta 1",
 				"Bia³ystok", "15-888", "999999999", new Date(), "1919111111");
-		//AdministratorSzczegoly administratorSzczegoly2 = new AdministratorSzczegoly("Jan", "Kowalski", "Prosta 1",
-			//	"WAWA", "15-888", "999999999", new Date(), "1919111111");
-		
+
 		administrator.setAdministratorSzczegoly(administratorSzczegoly);
 		administratorSzczegoly.setAdministrator(administrator);
-		
+
 		entityManager.persist(administrator);
 
 		entityManager.getTransaction().commit();
-		
-		
+
 		entityManager.close();
 		entityManagerFactory.close();
 
