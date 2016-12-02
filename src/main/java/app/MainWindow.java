7b822e7e42eb7df5dbf2363001f4a00412637627
Mateusz <mateuszbucko1@gmail.com>
2016-12-03 00:11:47 +0100

    Pobieranie wszystkich danych z kazdej tabeli

--- src/main/java/app/MainWindow.java
+++ src/main/java/app/MainWindow.java
@@ -1,6 +1,8 @@
 package app;
 import javax.swing.JPanel;
 
+import mapping.Administrator;
+
 public class MainWindow extends JPanel {
 
 	/**
@@ -11,6 +13,8 @@ public class MainWindow extends JPanel {
 	public static void main(String[] args) {	
 		@SuppressWarnings("unused")
 		View view = new View();
+		
+		
 	}
 
 }
