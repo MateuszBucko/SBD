b7cfecb9a9c8df42b487423d6781e3d3999aa79c
Mateusz <mateuszbucko1@gmail.com>
2016-12-08 00:33:58 +0100

    poprawki

--- src/main/java/dialogWindows/CheckRepairState.java
+++ src/main/java/dialogWindows/CheckRepairState.java
@@ -8,13 +8,12 @@ import java.util.ArrayList;
 import javax.persistence.EntityManager;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.DefaultListModel;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.ListModel;
+import javax.swing.JScrollPane;
 
 import app.DatabaseData;
 import app.Utils;
@@ -28,9 +27,9 @@ public class CheckRepairState {
 	JLabel stateLabel;
 	JLabel showStateLabel;
 	JComboBox productComboBox;
-	
+
 	JList statesList = new JList<String>();
-	
+
 	JPanel statePanel = new JPanel();
 
 	public CheckRepairState() {
@@ -42,90 +41,90 @@ public class CheckRepairState {
 		productComboBox = new JComboBox();
 		ArrayList<ReportedProduct> productList = DatabaseData.getAllReportedProducts();
 		productComboBox.setModel(new DefaultComboBoxModel(productList.toArray()));
-		
-		if(productList != null)
-		{
-			if(productList.size() > 0) productComboBox.setSelectedIndex(0);
+
+		if (productList != null) {
+			if (productList.size() > 0)
+				productComboBox.setSelectedIndex(0);
 		}
 
 		stateLabel = new JLabel("Stan: ");
 		showStateLabel = new JLabel();
-		
+
 		productComboBox.setPreferredSize(new Dimension(450, 50));
 		statePanel.add(productComboBox);
 		statePanel.add(statesList);
-		
+
+		statesList.add(new JScrollPane());
+
 		productComboBox.addItemListener(new ItemListener() {
-			
+
 			public void itemStateChanged(ItemEvent e) {
-				
+
 				String[] statesTable = new String[100];
 				statesList.setListData(statesTable);
 				int index = 0;
 				ReportedProduct selectedProduct = (ReportedProduct) productComboBox.getSelectedItem();
-				if(selectedProduct.getComplaints() == null) {
+				if (selectedProduct.getComplaints() == null) {
 					statesTable[0] = "Brak reklamacji dla tego przedmiotu";
 					statesList.setListData(statesTable);
-					if(selectedProduct.getComplaints().size() == 0)
-					{
+					if (selectedProduct.getComplaints().size() == 0) {
 						statesTable[0] = "Brak reklamacji dla tego przedmiotu";
 						statesList.setListData(statesTable);
 					}
-				}
-				else if(selectedProduct.getComplaints().size() > 1){
-					for(Complaint c : selectedProduct.getComplaints())
-					{
-						if(c.getDecision() != null)
-						{
-							if(c.getDecision().getIfPositive() != '\u0000')
-							{
+				} else if (selectedProduct.getComplaints().size() > 1) {
+					for (Complaint c : selectedProduct.getComplaints()) {
+						if (c.getDecision() != null) {
+							if (c.getDecision().getIfPositive() != '\u0000') {
 								String state = "";
-								state = showStateLabel.getText() + "ID: "+c.getComplaintId() + " Status: " ;
-								switch(c.getDecision().getIfPositive())
-								{
-								case MapConst.ACCEPTED: state += showStateLabel.getText()+" Zaakceptowana";break;
-								case MapConst.DECLINE: state += showStateLabel.getText()+" Odrzucona";break;
-								case MapConst.END: state += showStateLabel.getText()+" Zakończona";break;
-								case MapConst.FOR_RAPORT: state += showStateLabel.getText()+" Czeka na raport";break;
-								case MapConst.IN_SERVICE: state += showStateLabel.getText()+" W serwisie";break;
-								case MapConst.NEW: state += showStateLabel.getText()+" Nowa";break;
-								case MapConst.NEXT_REPAIR: state += showStateLabel.getText()+" Czeka na następną naprawę  ";break;
+								state = showStateLabel.getText() + "ID: " + c.getComplaintId() + " Status: ";
+								switch (c.getDecision().getIfPositive()) {
+								case MapConst.ACCEPTED:
+									state += showStateLabel.getText() + " Zaakceptowana";
+									break;
+								case MapConst.DECLINE:
+									state += showStateLabel.getText() + " Odrzucona";
+									break;
+								case MapConst.END:
+									state += showStateLabel.getText() + " Zakończona";
+									break;
+								case MapConst.FOR_RAPORT:
+									state += showStateLabel.getText() + " Czeka na raport";
+									break;
+								case MapConst.IN_SERVICE:
+									state += showStateLabel.getText() + " W serwisie";
+									break;
+								case MapConst.NEW:
+									state += showStateLabel.getText() + " Nowa";
+									break;
+								case MapConst.NEXT_REPAIR:
+									state += showStateLabel.getText() + " Czeka na następną naprawę  ";
+									break;
 								}
 								statesTable[index] = state;
 								index++;
 							}
 						}
 					}
-					System.out.println(checkIfEmpty(statesTable));
-					
-					
-						
-					
-					
 				}
-				if(checkIfEmpty(statesTable))
-				{
+				if (checkIfEmpty(statesTable)) {
 					statesTable[0] = "Brak reklamacji dla tego przedmiotu";
 				}
 				statesList.setListData(statesTable);
-				
+
 			}
 		});
-		
+
 		checkRepairState.add(statePanel);
-		
+
 		checkRepairState.setBounds(100, 100, 450, 250);
 		checkRepairState.setVisible(true);
-		
 
 	}
-	
-	private boolean checkIfEmpty(String [] tab)
-	{
-		for(int i =0 ; i <tab.length;i++)
-		{
-			System.out.println(tab[i]);
-			if(tab[i] != null && !tab[i].equals("")) return false;
+
+	private boolean checkIfEmpty(String[] tab) {
+		for (int i = 0; i < tab.length; i++) {
+			if (tab[i] != null && !tab[i].equals(""))
+				return false;
 		}
 		return true;
 	}
